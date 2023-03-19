1e1f1a0edd95412b66f105ca0948b54fc52185b4
Hsun <hsun1031@outlook.com>
2023-03-19 16:15:28 +0800

    🔧 refactor: Set `I2C` and `LEDC` `frequency`
    Set I2C frequency to 100000UL, LEDC frequency to 100000UL.

--- src/components/module/module.cpp
+++ src/components/module/module.cpp
@@ -4,8 +4,8 @@
 #include "components/module.h"
 
 void setup_module(Cleaner_module_conf *cleaner_module_conf) {
-    Wire. begin(CLEANER_DEFAULT_I2C_SDA,  CLEANER_DEFAULT_I2C_SCL);
-    Wire1.begin(CLEANER_DEFAULT_I2C2_SDA, CLEANER_DEFAULT_I2C2_SCL);
+    Wire. begin(CLEANER_DEFAULT_I2C_SDA,  CLEANER_DEFAULT_I2C_SCL,  CLEANER_DEFAULT_I2C_FREQ);
+    Wire1.begin(CLEANER_DEFAULT_I2C2_SDA, CLEANER_DEFAULT_I2C2_SCL, CLEANER_DEFAULT_I2C_FREQ);
 
     setup_MX1584(&(cleaner_module_conf->mx1584_pinout_conf), &(cleaner_module_conf->mx1584_pwm_conf));
     setup_IRF520(cleaner_module_conf->IRF520_pin);
